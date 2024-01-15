1ef0c03e46f42431a68c8a114c334ae96302b329
Nadia <wzmjohn2018@163.com>
2024-01-16 00:52:40 +0800

    refactor: convert performUnitOfWork to fuctions

--- d02/v04/vite-runner/core/React.js
+++ d02/v04/vite-runner/core/React.js
@@ -29,23 +29,6 @@ function render(el, container) {
             children: [el]
         }
     }
-    // const dom = el.type === 'TEXT_ELEMENT'
-    //     ? document.createTextNode("")
-    //     : document.createElement(el.type)
-
-    // //处理props
-    // Object.keys(el.props).forEach((key) => {
-    //     if (key !== 'children') {
-    //         dom[key] = el.props[key]
-    //     }
-    // })
-    // //处理children
-    // const children = el.props.children;
-    // // console.log('children type:', typeof(children))
-    // children.forEach((child) => {
-    //     render(child, dom)
-    // })
-    // container.append(dom)
 }
 let nextWorkOfUnit = null
 function workLoop(deadline) {
@@ -56,50 +39,94 @@ function workLoop(deadline) {
     }
     requestIdleCallback(workLoop)
 }
-function performWorkOfUnit(work) {
-    // 1. 创建DOM
-    if (!work.dom) {
-        const dom = (work.dom = work.type === 'TEXT_ELEMENT'
-            ? document.createTextNode("")
-            : document.createElement(work.type))
-        work.parent.dom.append(dom)
-        // 2. 处理props
-        Object.keys(work.props).forEach((key) => {
-            if (key !== 'children') {
-                dom[key] = work.props[key]
-            }
-        })
-    }
+function createDom(type) {
+    return type === "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(type);
+}
 
-    // 3. 转换成Linear，设置好指针
-    const children = work.props.children
-    let prevChild = null
+function updateProps(dom, props) {
+    Object.keys(props).forEach((key) => {
+        if (key !== "children") {
+            dom[key] = props[key];
+        }
+    });
+}
+
+function initChildren(fiber) {
+    const children = fiber.props.children;
+    let prevChild = null;
     children.forEach((child, index) => {
-        const newWork = {
+        const newFiber = {
             type: child.type,
             props: child.props,
             child: null,
-            parent: work,
+            parent: fiber,
             sibling: null,
-            dom: null
+            dom: null,
+        };
 
-        }
         if (index === 0) {
-            work.child = newWork
+            fiber.child = newFiber;
         } else {
-            prevChild.sibling = newWork
+            prevChild.sibling = newFiber;
         }
-        prevChild = newWork
+        prevChild = newFiber;
+    });
+}
+function performWorkOfUnit(fiber) {
+    if (!fiber.dom) {
+        const dom = (fiber.dom = createDom(fiber.type));
 
-    })
+        fiber.parent.dom.append(dom);
+
+        updateProps(dom, fiber.props);
+    }
+
+    initChildren(fiber)
+    // // 1. 创建DOM
+    // if (!work.dom) {
+    //     const dom = (work.dom = work.type === 'TEXT_ELEMENT'
+    //         ? document.createTextNode("")
+    //         : document.createElement(work.type))
+    //     work.parent.dom.append(dom)
+    //     // 2. 处理props
+    //     Object.keys(work.props).forEach((key) => {
+    //         if (key !== 'children') {
+    //             dom[key] = work.props[key]
+    //         }
+    //     })
+    // }
+
+    // // 3. 转换成Linear，设置好指针
+    // const children = work.props.children
+    // let prevChild = null
+    // children.forEach((child, index) => {
+    //     const newWork = {
+    //         type: child.type,
+    //         props: child.props,
+    //         child: null,
+    //         parent: work,
+    //         sibling: null,
+    //         dom: null
+
+    //     }
+    //     if (index === 0) {
+    //         work.child = newWork
+    //     } else {
+    //         prevChild.sibling = newWork
+    //     }
+    //     prevChild = newWork
+
+    // })
     // 4. 返回下一个要执行的任务 
-    if (work.child) {
-        return work.child
+    if (fiber.child) {
+        return fiber.child
     }
-    if (work.sibling) {
-        return work.sibling
+    if (fiber.sibling) {
+        return fiber.sibling
     }
-    return work.parent?.sibling
+    return fiber.parent?.sibling
 }
 requestIdleCallback(workLoop)
 
