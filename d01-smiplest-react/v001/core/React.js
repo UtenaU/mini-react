9a7a7711350fcffb373d3566424922a292bcdb81
Nadia <wzmjohn2018@163.com>
2024-01-15 10:10:26 +0800

    refactor: Reactify

---
+++ d01-smiplest-react/v001/core/React.js
@@ -0,0 +1,50 @@
+//type props children
+//object express
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map((child) => {
+                return typeof child === 'string' ? createTextNode(child) : child
+            })
+        }
+    }
+}
+
+function createTextNode(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        }
+    };
+}
+
+function render(el, container) {
+    const dom = el.type === 'TEXT_ELEMENT'
+        ? document.createTextNode("")
+        : document.createElement(el.type)
+
+    //处理props
+    Object.keys(el.props).forEach((key) => {
+        if (key !== 'children') {
+            dom[key] = el.props[key]
+        }
+    })
+    //处理children
+    const children = el.props.children;
+    // console.log('children type:', typeof(children))
+    children.forEach((child) => {
+        render(child, dom)
+    })
+    container.append(dom)
+}
+
+const React = {
+    render,
+    createElement,
+}
+
+export default React
--- d01-smiplest-react/v001/main.js
+++ d01-smiplest-react/v001/main.js
@@ -1,48 +1,6 @@
-//type props children
-//object express
-function createElement(type, props, ...children) {
-    return {
-        type,
-        props: {
-            ...props,
-            children: children.map((child) => {
-                return typeof child === 'string' ? createTextNode(child) : child
-            })
-        }
-    }
-}
+import ReactDOM from './core/ReactDOM.js'
+import App from "./App.js";
+// render(App, document.querySelector('#root'))
+ReactDOM.createRoot(document.querySelector('#root')).render(App)
 
-function createTextNode(text) {
-    return {
-        type: "TEXT_ELEMENT",
-        props: {
-            nodeValue: text,
-            children: [],
-        }
-    };
-}
 
-function render(el, container) {
-    const dom = el.type === 'TEXT_ELEMENT'
-        ? document.createTextNode("")
-        : document.createElement(el.type)
-
-    //处理props
-    Object.keys(el.props).forEach((key) => {
-        if (key !== 'children') {
-            dom[key] = el.props[key]
-        }
-    })
-    //处理children
-    const children = el.props.children;
-    // console.log('children type:', typeof(children))
-    children.forEach((child) => {
-        render(child, dom)
-    })
-    container.append(dom)
-}
-
-// const textEl = createTextNode("app")
-const App = createElement("div", { id: "app" }, 'mini-', 'react')
-
-render(App, document.querySelector('#root'))
