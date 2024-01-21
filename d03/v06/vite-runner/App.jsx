7f89a2a982133a55c35ddfa6e87d1dea7e1412f1
Nadia <wzmjohn2018@163.com>
2024-01-21 18:02:30 +0800

    bug:infinite loop of null before right render and do not render function

---
+++ d03/v06/vite-runner/App.jsx
@@ -0,0 +1,13 @@
+import React from "./core/React.js";
+// const App = React.createElement("div", { id: "app" }, 'hello,','mini-', 'react','-world')
+function Counter(){
+    return <div>count</div>
+}
+const App = <div>hello-mini-rct
+    <Counter></Counter>
+</div>
+// function App(){
+//     return <div>xxx,mini-react</div>
+// }
+// console.log(AppOne)
+export default App
---
+++ d03/v06/vite-runner/core/React.js
@@ -0,0 +1,143 @@
+//type props children
+//object express
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map((child) => {
+                return typeof child === 'string' ? createTextNode(child) : child
+            })
+        }
+    }
+}
+
+function createTextNode(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        }
+    };
+}
+
+function render(el, container) {
+    nextWorkOfUnit = {
+        dom: container,
+        props: {
+            children: [el]
+        }
+    }
+    root = nextWorkOfUnit
+}
+let root = null
+let nextWorkOfUnit = null
+function workLoop(deadline) {
+    let shouldYield = false;
+    while (!shouldYield && nextWorkOfUnit) {
+        // if(!root){
+        //     root = nextWorkOfUnit
+        // }
+        nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+        shouldYield = deadline.timeRemaining < 1;
+    }
+    if (!nextWorkOfUnit) {
+        // 得到所有的dom都生成了，我们再去统一提交给DOM树去把他们添加上去
+        // 即，有了链表之后要从头开始遍历该链表，将其append to dom tree
+        // 所以，我们需要有链表头,在所有dom都生成后，统一提交链表头的添加，然后伴随着线性的后续dom元素提交 
+        commitRoot(root)
+    }
+    requestIdleCallback(workLoop)
+}
+function commitRoot(root) {
+    // fiber.parent.dom.append(fiber.dom)
+    commitWork(root.child)
+    //处理子节点
+    root = null
+}
+function commitWork(fiber) {
+    if (!fiber) return
+    let fiberParent = fiber.parent
+    if (!fiberParent.dom) {
+        fiberParent = fiberParent.parent
+    }
+    fiber.parent.dom.append(fiber.dom)
+    commitWork(fiber.child)
+    commitWork(fiber.sibling)
+}
+function createDom(type) {
+    console.log(type)
+    return type === "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(type);
+}
+
+function updateProps(dom, props) {
+    Object.keys(props).forEach((key) => {
+        if (key !== "children") {
+            dom[key] = props[key];
+        }
+    });
+}
+
+function initChildren(fiber) {
+    const children = fiber.props.children;
+    let prevChild = null;
+    children.forEach((child, index) => {
+        const newFiber = {
+            type: child.type,
+            props: child.props,
+            child: null,
+            parent: fiber,
+            sibling: null,
+            dom: null,
+        };
+
+        if (index === 0) {
+            fiber.child = newFiber;
+        } else {
+            prevChild.sibling = newFiber;
+        }
+        prevChild = newFiber;
+    });
+}
+function performWorkOfUnit(fiber) {
+    const isFuctionComponent = typeof fiber.type === "function"
+    if (!isFuctionComponent) {
+        if (!fiber.dom) {
+            const dom = (fiber.dom = createDom(fiber.type));
+
+            // fiber.parent.dom.append(dom);
+
+            updateProps(dom, fiber.props);
+        }
+    }
+
+    const children = isFuctionComponent ? [fiber.type()] : fiber.props.children
+    initChildren(fiber, children)
+
+    if (fiber.child) {
+        return fiber.child
+    }
+    if (fiber.sibling) {
+        return fiber.sibling
+    }
+    return fiber.parent?.sibling
+}
+requestIdleCallback(workLoop)
+/**
+ * 如果 main.js 文件引用了 React，并且 react.js 文件中的 requestIdleCallback(workLoop) 是在 main.js 之后执行的，
+ * 那么它将共享同一个全局上下文。
+ * 在这种情况下，requestIdleCallback 会生效，因为它注册的回调函数 workLoop 将在浏览器的空闲时段内执行。
+
+这是因为全局上下文是一个共享的环境，requestIdleCallback 注册的回调函数是在全局上下文中执行的。
+只要 requestIdleCallback 的调用发生在 workLoop 实际需要执行之前，并且在同一个全局上下文中，它就会生效
+ */
+
+const React = {
+    render,
+    createElement,
+}
+
+export default React
